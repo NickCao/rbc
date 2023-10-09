d214fbe8b2352c83ef963041c6da51a4b122c4c9
Nick Cao <nickcao@nichi.co>
2023-10-08 22:55:21 -0400

    work

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use clap::Parser;
+use nom::bytes::complete::is_not;
 use nom::bytes::complete::tag;
+use nom::bytes::complete::take_till;
+use nom::character::complete::alpha1;
+use nom::character::complete::alphanumeric1;
 use nom::character::complete::newline;
+use nom::character::complete::one_of;
 use nom::character::complete::space1;
 use nom::character::complete::u64;
+use nom::character::is_newline;
 use nom::combinator::map;
 use nom::multi::count;
+use nom::multi::many0;
 use nom::sequence::delimited;
 use nom::sequence::terminated;
 use nom::sequence::{preceded, tuple};
@@ -28,6 +35,13 @@ struct Header {
     o: u64,
     /// A = number of AND gates
     a: u64,
+}
+
+#[derive(Debug)]
+struct Symbol {
+    kind: char,
+    variable: u64,
+    identifier: String,
 }
 
 #[derive(Debug)]
@@ -112,6 +126,30 @@ fn parse_gate(input: &[u8]) -> IResult<&[u8], Gate> {
     )(input)
 }
 
+fn parse_symbol(input: &[u8]) -> IResult<&[u8], Symbol> {
+    terminated(
+        map(tuple(()), |()| Symbol {
+            kind: 'a',
+            variable: 0,
+            identifier: "test".to_string(),
+        }),
+        newline,
+    )(input)
+    /*
+    terminated(
+        map(
+            tuple((alphanumeric1, space1, alphanumeric1)),
+            |(kind, _, identifier)| Symbol {
+                kind,
+                variable,
+                identifier: identifier.to_string(),
+            },
+        ),
+        newline,
+    )(input)
+    */
+}
+
 fn aig(input: &[u8]) -> IResult<&[u8], Header> {
     let h = header(input)?;
     let d = tuple((
@@ -119,6 +157,7 @@ fn aig(input: &[u8]) -> IResult<&[u8], Header> {
         count(parse_latch, h.1.l.try_into().unwrap()),
         count(parse_output, h.1.o.try_into().unwrap()),
         count(parse_gate, h.1.a.try_into().unwrap()),
+        many0(parse_symbol),
     ))(h.0)?;
     dbg!(d);
     Ok(h)
