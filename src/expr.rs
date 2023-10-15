f72922472d3da3909cfb42d74cdfbf1c4c3cc4b9
Nick Cao <nickcao@nichi.co>
2023-10-14 23:34:09 -0400

    properly test ast parsing

--- src/expr.rs
+++ src/expr.rs
@@ -1,7 +1,8 @@
 use lalrpop_util::lalrpop_mod;
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum Expr {
-    Term(String),
+    Term(char),
     And(Box<Expr>, Box<Expr>),
     Or(Box<Expr>, Box<Expr>),
     Not(Box<Expr>),
@@ -11,7 +12,22 @@ lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
 
 #[test]
 fn calculator1() {
-    assert!(calculator1::ExprParser::new().parse("A").is_ok());
-    assert!(calculator1::ExprParser::new().parse("(A+B)").is_ok());
-    assert!(calculator1::ExprParser::new().parse("((A*B)+C)").is_ok());
+    assert_eq!(
+        calculator1::ExprParser::new().parse("A").unwrap(),
+        Box::new(Expr::Term('A'))
+    );
+    assert_eq!(
+        calculator1::ExprParser::new().parse("(A+B)").unwrap(),
+        Box::new(Expr::Or(
+            Box::new(Expr::Term('A')),
+            Box::new(Expr::Term('B'))
+        ))
+    );
+    assert_eq!(
+        calculator1::ExprParser::new().parse("(A*B)").unwrap(),
+        Box::new(Expr::And(
+            Box::new(Expr::Term('A')),
+            Box::new(Expr::Term('B'))
+        ))
+    );
 }
