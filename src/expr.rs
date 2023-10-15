136ddad9b67cd845f508f4f4fc184cde167bfebc
Nick Cao <nickcao@nichi.co>
2023-10-15 10:24:12 -0400

    convert box into aig

--- src/expr.rs
+++ src/expr.rs
@@ -1,12 +1,40 @@
+use crate::aig;
 use lalrpop_util::lalrpop_mod;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Expr {
     Term(char),
     Not(Box<Expr>),
     Or(Box<Expr>, Box<Expr>),
     And(Box<Expr>, Box<Expr>),
     Xor(Box<Expr>, Box<Expr>),
+}
+
+impl Into<Box<aig::AIG>> for Box<Expr> {
+    fn into(self) -> Box<aig::AIG> {
+        match *self {
+            Expr::Term(v) => (v as usize - 'A' as usize).into(),
+            Expr::Not(v) => {
+                let v: Box<aig::AIG> = v.into();
+                !v
+            }
+            Expr::And(l, r) => {
+                let l: Box<aig::AIG> = l.into();
+                let r: Box<aig::AIG> = r.into();
+                l & r
+            }
+            Expr::Or(l, r) => {
+                let l: Box<aig::AIG> = l.into();
+                let r: Box<aig::AIG> = r.into();
+                !((!l) & (!r))
+            }
+            Expr::Xor(l, r) => Box::new(Expr::Or(
+                Box::new(Expr::And(l.clone(), Box::new(Expr::Not(r.clone())))),
+                Box::new(Expr::And(r.clone(), Box::new(Expr::Not(l.clone())))),
+            ))
+            .into(),
+        }
+    }
 }
 
 lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
